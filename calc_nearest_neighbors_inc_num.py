5251b2011952b22fa8b7b9f480e653f99bebfc78
Morgan Nance <morganlnance@gmail.com>
2017-05-05 12:31:36 -0400

    used the functionality from push_string.py to make the nn inc_num code work with images that have nvars variables

--- calc_nearest_neighbors_inc_num.py
+++ calc_nearest_neighbors_inc_num.py
@@ -3,7 +3,9 @@ __author__="morganlnance"
 
 
 '''
-Usage: python <script.py> /path/to/string_<>.dat
+Usage: python <script>.py string_cycle#.dat nvars cycle_number
+Arguments: string_cycle#.dat (/path/to/the string.dat file)
+           nvars             (the number of variables defining one image)
 
 Take each image ii along a string, in sequence
 Calculate the distance between ii and every other image
@@ -59,46 +61,97 @@ def calc_dist( p1, p2 ):
 #####################
 # READ-IN ARGUMENTS #
 #####################
-# read-in and check the string_<>.dat file
+# 1) read-in and check the string_<>.dat file
 try:
     string_file = sys.argv[1]
     # ensure this is a valid path
     if not os.path.isfile( string_file ):
-        print "\nI need a valid string_<>.dat file."
-        print "\n%s is not a valid path.\n" %string_file
-        sys.exit()
-    # read the data from the string_file
-    try:
-        string_f = open( string_file, 'r' )
-        string_data = string_f.readlines()
-        string_f.close()
-    except:
-        print "\nI had an issue opening and reading your string file.\n"
+        print "\nI need a valid string_cycle#.dat file."
+        print "'%s' is not a valid path.\n" %string_file
         sys.exit()
 # if no input argument was given
 except IndexError:
-    print "\nI need a string_<>.dat file.\n"
+    print "\nI need a string_cycle#.dat file.\n"
     sys.exit()
-
-
-#############################
-# PARSE THROUGH STRING DATA #
-#############################
-## collect the string phi,psi data from the file
-# ignore commented lines in the string file (# Image n)
-# phi,psi data is repeated in order per image
+# 2) read-in and check nvars argument
 try:
-    string_data = [ float( l.strip() ) for l in string_data
-                    if not l.startswith( '#' ) ]
-except:
-    print "\nSomething is wrong with your string_<>.dat file."
-    print "Are there lines in there that need to be commented out?\n"
+    nvars = sys.argv[2]
+    # ensure it is a number
+    try:
+        nvars = int(float( nvars ))
+    # if not a number        
+    except ValueError:
+        print "\nYou did not give me a number as your nvars argument.\n"
+        sys.exit()
+# if no nvars was given
+except IndexError:
+    print "\nYou did not give me an nvars argument.\n"
     sys.exit()
-# collect phi,psi data in tuples per image
-string_phi_psi = zip( string_data[::2], 
-                      string_data[1::2] )
-# there are as many images as phi,psi tuples
-nimages = len( string_phi_psi )
+
+
+#######################
+# COLLECT IMAGES VARS #
+#######################
+# read and store the image number vars
+# from the string_cycle#.dat file
+try:
+    # nucleus cluster runs on older python
+    # no with open statements allowed
+    fh = open( string_file, 'r' )
+    lines = fh.readlines()
+    fh.close()
+except IOError:
+    print "\nI couldn't open your string_cycle#.dat file.\n" \
+        "Is there something wrong with %s ?\n" %string_file
+    sys.exit()
+# parse the file, storing vars info
+all_images_raw = [ float( line.strip() )
+                   for line in lines
+                   if not line.startswith( '#' ) ] # if not '# Image n'
+# string_cycle#.dat file looks like
+'''
+# Image n
+phi
+psi
+dist
+length
+...
+# Image n+1
+phi
+psi
+dist
+length
+...
+'''
+## each image is described by nvars variables
+## so each variable associated with each image
+## is repeated in a predictable manner
+## ex) alad project described by phi,psi
+## ex) where phi came first then psi after each
+## '# Image n' line
+# so loop over the all_images_raw data according to how
+# many variables describe the data (nvars)
+all_images = []
+for ii in range( nvars ):
+    # each var that describes the image repeats in
+    # a predictable manner
+    # so by sorting through all the info from the dat
+    # file in a way that parses through it in repeating
+    # chunks means that all variables can be pulled out
+    # in their corresponding manner
+    # reminder: list[start_at:end_before:skip]
+    # so start_at should iterate from 0 to nvars
+    # and nvars should be skipped each time
+    all_images.append( all_images_raw[ii::nvars] )
+# unpack and organize the data appropriately
+# each tuple in the data list are the variables associated
+# with that particular image number (by index)
+all_images = zip( *all_images )
+
+# the number of images is the number of
+# data point tuples from the file
+nimages = len( all_images )
+
 
 
 ###############################
@@ -116,17 +169,17 @@ nimages = len( string_phi_psi )
 nn_list = [ 0 ]
 ii = 0
 while ii != ( nimages - 1 ):
-    # pull out the phi,psi tuple for that image number
-    ii_phi_psi = string_phi_psi[ ii ]
+    # pull out the variables for that image number
+    image_vars = all_images[ii]
 
-    # this keeps track of distances between image
-    # ii and all other images jj
+    # this keeps track of distances between image ii
+    # and all other images jj
     # ignore the image number we are currently using (jj!=ii)
-    # use image jj to pull out the phi,psi tuple from
-    # the string_phi_psi data holder and use it to
+    # use image jj to pull out the image variables from
+    # the all_images data holder and use it to
     # calc_dist between image ii and image jj
-    distances, image_numbers = zip( *[ (calc_dist( ii_phi_psi, 
-                                                   string_phi_psi[ jj ] ), 
+    distances, image_numbers = zip( *[ (calc_dist( image_vars, 
+                                                   all_images[jj] ), 
                                         jj )
                                        for jj in range( nimages )
                                        if not jj == ii ] )
@@ -156,10 +209,19 @@ while ii != ( nimages - 1 ):
     # this is our new, growing string
     nn_list.append( nn )
 
-# write out the new string
-# string_phi_psi = ( phi, psi )
-# so phi is the first element and psi is the second
-for img, ii in zip( nn_list, range( len( nn_list ) )):
-    print "# Image %s\n%s\n%s" %( ii,
-                                  string_phi_psi[img][0], 
-                                  string_phi_psi[img][1] )
+
+###################
+# CREATE DAT FILE #
+###################
+# convert the nearest neighbors data to a new string
+for ii in range( len( nn_list ) ):
+    # get the corresponding nearest-neighbor image number
+    image_num = nn_list[ii]
+    # pull out the image
+    image = all_images[ image_num ]
+    # print the format for this image
+    print "# Images %s" %ii
+    # print all the data describing this image
+    # len( image ) should == nvars
+    for jj in image:
+        print jj
